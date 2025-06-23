36b52bc072549b93bf1fb69a62e3c8f254ced87e
VaricelaZoster <dwaipayansahastudent2@gmail.com>
2025-06-23 15:08:46 +0530

    Added generateURL page

--- app/selectURL/page.js
+++ app/selectURL/page.js
@@ -1,10 +1,44 @@
-import React from 'react'
+'use client'
+import LinkInput from '@/components/LinkInput'
+import React, { useState, useEffect } from 'react'
 
+const SelectURL = () => {
+  const [user, setUser] = useState('');
+  const [inputCount, setInputCount] = useState(1); 
 
-const selectURL = () => {
+  useEffect(() => {
+    const username = localStorage.getItem("username") || '';
+    setUser(username);
+  }, []);
+
+  const addInput = () => {
+    if (inputCount < 6) {
+      setInputCount(prev => prev + 1);
+    }
+  }
+
   return (
-    <div>selectURL</div>
-  )
-}
+    <div className='flex flex-col h-screen bg-amber-200 justify-center items-center'>
+      <div className='text-4xl font-extrabold pb-4'>
+        Enter your links {user.toUpperCase()}
+      </div>
 
-export default selectURL
+      <div className='flex flex-col gap-4'>
+        {[...Array(inputCount)].map((_, index) => (
+          <div key={index} className='flex gap-2 items-center'>
+            <LinkInput />
+            {index === inputCount - 1 && (
+              <button
+                onClick={addInput}
+              >
+                +
+              </button>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SelectURL;
---
+++ components/LinkInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LinkInput = () => {
+  return (
+    <div className='flex'>
+      <div>URL</div>
+      <input className='bg-white' type='url'></input>
+    </div>
+  )
+}
+
+export default LinkInput
