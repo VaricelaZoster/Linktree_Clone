27124ef811881d85206d7f21e47ab140b2f23e98
VaricelaZoster <dwaipayansahastudent2@gmail.com>
2025-06-24 21:35:20 +0530

    Designed the generateURL page

--- app/selectURL/page.js
+++ app/selectURL/page.js
@@ -1,10 +1,14 @@
 'use client'
 import LinkInput from '@/components/LinkInput'
 import React, { useState, useEffect } from 'react'
+import './page.css'
+
 
 const SelectURL = () => {
   const [user, setUser] = useState('');
   const [inputCount, setInputCount] = useState(1); 
+  const [showError, setShowError] = useState(false);
+  const [isShaking, setIsShaking] = useState(false);
 
   useEffect(() => {
     const username = localStorage.getItem("username") || '';
@@ -14,11 +18,20 @@ const SelectURL = () => {
   const addInput = () => {
     if (inputCount < 6) {
       setInputCount(prev => prev + 1);
+      setShowError(false);
+    } else {
+      setShowError(true);
+      setIsShaking(true);
+
+      // Remove shake class after animation
+      setTimeout(() => {
+        setIsShaking(false);
+      }, 500); // match animation duration
     }
-  }
+  };
 
   return (
-    <div className='flex flex-col h-screen bg-amber-200 justify-center items-center'>
+    <div className='flex flex-col h-screen justify-center items-center'>
       <div className='text-4xl font-extrabold pb-4'>
         Enter your links {user.toUpperCase()}
       </div>
@@ -30,6 +43,7 @@ const SelectURL = () => {
             {index === inputCount - 1 && (
               <button
                 onClick={addInput}
+                className='border-2 bg-[rgb(246,247,245)] border-gray-400 text-gray-500 flex justify-center align-middle h-12 w-12 rounded-md text-3xl'
               >
                 +
               </button>
@@ -37,6 +51,16 @@ const SelectURL = () => {
           </div>
         ))}
       </div>
+
+      {showError && (
+        <div className={`pt-3 text-red-400 ${isShaking ? 'shake' : ''}`}>
+          Cannot add more links
+        </div>
+      )}
+
+      <button className='mt-10 p-3 w-1/2 rounded-full text-white font-extrabold text-xl bg-purple-500 cursor-pointer hover:bg-purple-700'>
+        Continue
+      </button>
     </div>
   );
 };
--- components/LinkInput.jsx
+++ components/LinkInput.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { MdInsertLink, MDInsertLink } from 'react-icons/md'
+import { Tooltip } from '@mui/material'
 
 const LinkInput = () => {
   return (
-    <div className='flex'>
-      <div>URL</div>
-      <input className='bg-white' type='url'></input>
+    <div className='flex h-12 gap-4'>
+      <Tooltip disableInteractive placement='top' title={
+        <React.Fragment>
+          <div className='text-[0.8rem]'>Insert URL Here</div>
+        </React.Fragment>
+      }>
+        <div className='border-gray-400 rounded-md border-2 flex w-12 text-xl text-gray-500 justify-center items-center bg-[rgb(246,247,245)] '><MdInsertLink/></div>
+      </Tooltip>
+      <input placeholder='Enter display text here' className='bg-[rgb(246,247,245)] px-2 border-2 border-gray-400 rounded-md ' type='url'></input>
     </div>
   )
 }
