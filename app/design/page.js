8bace2539bc269cbfb21edd589d1f57ee5fc80f9
VaricelaZoster <dwaipayansahastudent2@gmail.com>
2025-07-02 14:52:22 +0530

    Almost done

--- app/design/page.js
+++ app/design/page.js
@@ -2,14 +2,30 @@
 
 import { useRouter, useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
 
 const Design = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const user = searchParams.get('user')
+  const copy = searchParams.get('copy')
 
   const [userExists, setUserExists] = useState(null)
   const [links, setLinks] = useState([])
+
+  const copying = () => {
+    const url = new URL(window.location.href)
+    url.searchParams.delete('copy')
+
+    navigator.clipboard.writeText(url.toString())
+      .then(() => {
+        toast.success('Copied!!',{
+          autoClose: 1500,
+          pauseOnHover: false
+        })
+      })
+      .catch(err => console.error('Copy failed', err));
+  }
 
   useEffect(() => {
     async function verify(user) {
@@ -56,30 +72,35 @@ const Design = () => {
   return (
     <div className="flex justify-center items-center bg-[rgb(250,249,254)] h-screen">
       <div className="flex flex-col justify-center items-center h-screen sm:h-200 w-120">
-        <div className="flex text-[rgb(184,175,243)] border-x-2 border-t-2 border-t-[rgb(184,175,243)] border-x-[rgb(184,175,243)] h-80 w-full justify-center items-center text-5xl font-extrabold rounded-t-3xl p-4">
+        <div className="flex text-[rgb(184,175,243)] border-x-2 border-t-2 border-t-[rgb(160,150,228)] border-x-[rgb(160,150,228)] h-80 w-full justify-center items-center text-5xl font-extrabold rounded-t-3xl p-4">
           {user.toUpperCase()}
         </div>
-        <div className="bg-amber-500 h-220 w-full rounded-b-3xl p-4 overflow-y-auto">
+        <div className="border-x-2 border-b-2 border-b-[rgb(160,150,228)] border-x-[rgb(160,150,228)] h-220 w-full rounded-b-3xl p-4 overflow-y-auto">
           {links.length > 0 ? (
-            <ul className="space-y-2">
+            <ul className="flex flex-col gap-7">
               {links.map((link, index) => (
-                <li key={index} className='bg-white'>
-                  <a
-                    href={link.URL}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-800 underline hover:text-blue-600"
-                  >
-                    {link.Text}
-                  </a>
-                </li>
+                <a
+                  key={index}
+                  href={link.URL}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-xl border-2 border-[rgb(160,150,228)] rounded-full bg-white h-15 text-center text-[rgb(141,125,241)] flex justify-center items-center"
+                >
+                  {link.Text.toUpperCase()}
+                </a>
               ))}
             </ul>
           ) : (
             <p>No links available</p>
           )}
         </div>
+        {
+          copy == 'true' && (
+            <div onClick={() => copying()} className='flex m-3 cursor-pointer text-md text-[rgb(141,125,241)]'>Copy!</div>
+          )
+        }
       </div>
+      <ToastContainer/>
     </div>
   )
 }
